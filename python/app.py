86a31b71fdb53ffa7034d8c42cbadd2e52610507
JatinRexwal <jtrexable@gmail.com>
2025-03-31 10:28:01 +0530

    python_01

---
+++ python/app.py
@@ -0,0 +1,22 @@
+# var = "Jim"
+# print(var)
+
+#Dictonary and List#
+
+list = {"name1":"John","name2":"Tim","name3":"Jim","name4":"James","Female":["Daisy","Anna","Donna"],"name5":"Rob"}
+print(list["Female"][0])
+
+list = {
+    "name1":"John",
+    "name2":"Tim",
+    "name3":"Jim",
+    "name4":"James",
+    "Female":[
+        "Daisy",
+        "Anna",
+        "Donna"
+        ],
+    "name5":"Rob"
+    }
+print(list["Female"][2])
+
